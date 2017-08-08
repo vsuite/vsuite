4c4455c3118f0393154ab58c5ce04ef7055d1100
blackcater <blackcater2015@gmail.com>
2017-08-08 09:57:15 +0800

    【修改】项目结构
    - 配置文件 移入 build 目录下
    - 修改 .editorconfig 配置文件

--- build/config.js
+++ build/config.js
@@ -1,12 +1,16 @@
+const path = require('path')
+
 module.exports = {
   dev: {
     env: {
-      NODE_ENV: 'development',
+      NODE_ENV: '"development"',
     },
+    outputPath: path.resolve(__dirname, '../test'),
   },
   pro: {
     env: {
-      NODE_ENV: 'production',
+      NODE_ENV: '"production"',
     },
+    outputPath: path.resolve(__dirname, '../dist'),
   },
 }
---
+++ build/webpack.config.basic.js
@@ -0,0 +1,97 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const config = require('./config')
+
+const srcPath = path.resolve(__dirname, 'src')
+const outputPath = process.env.NODE_ENV === 'development' ? config.dev.outputPath : config.pro.outputPath
+
+const extractLight = new ExtractTextPlugin(path.resolve(outputPath, 'css/wibi.css'))
+const extractDark = new ExtractTextPlugin(path.resolve(outputPath, 'css/wibi-dark.css'))
+const extractTypo = new ExtractTextPlugin(path.resolve(outputPath, 'css/wibi-typo.css'))
+
+const extractStylusOptions = {
+  fallback: 'style-loader',
+  use: [
+    'css-loader',
+    {
+      loader: 'stylus-loader',
+      options: {
+        use: [],
+      },
+    },
+  ],
+}
+
+module.exports = {
+  entry: {
+    index: path.resolve(srcPath, 'scripts/index.js'),
+  },
+  output: {
+    filename: "js/wibi.js",
+    path: outputPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'xo-loader',
+        enforce: 'pre',
+        include: [path.resolve(srcPath, 'scripts')],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: [path.resolve(srcPath, 'scripts')],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader',
+        include: [path.resolve(srcPath, 'pages')],
+      },
+      {
+        test: /^light\.styl$/,
+        use: extractLight.extract(extractStylusOptions),
+      },
+      {
+        test: /^dark\.styl$/,
+        use: extractDark.extract(extractStylusOptions),
+      },
+      {
+        test: /^typo\.styl$/,
+        use: extractTypo.extract(extractStylusOptions),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
+        use: `file-loader?name=[name].[ext]&outputPath=${path.resolve(outputPath, 'images')}`,
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: `file-loader?name=[name].[ext]&outputPath=${path.resolve(outputPath, 'fonts')}`,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      scripts: path.resolve(srcPath, 'scripts'),
+      styles: path.resolve(srcPath, 'styles'),
+      pages: path.resolve(srcPath, 'pages'),
+      images: path.resolve(srcPath, 'images'),
+      fonts: path.resolve(srcPath, 'fonts'),
+    },
+    extensions: ['.js', '.styl'],
+    modules: [srcPath, 'node_modules'],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    extractLight,
+    extractDark,
+    extractTypo,
+  ]
+}
