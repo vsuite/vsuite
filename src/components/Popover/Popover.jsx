4120a9a1d1141a0b2bee20e216f0b68577c2291c
blackcater <blackcater2015@gmail.com>
2018-08-06 22:17:18 +0800

    feat: finish add popper.js

--- src/components/Popover/Popover.jsx
+++ src/components/Popover/Popover.jsx
@@ -4,7 +4,19 @@ import Popper from 'popper.js';
 export default {
   name: 'Popover',
 
+  model: {
+    prop: 'visible',
+    event: 'change',
+  },
+
   props: {
+    visible: {
+      type: Boolean,
+      default: undefined,
+    },
+    defaultVisible: VueTypes.bool.def(false),
+    disabled: VueTypes.bool.def(false),
+    always: VueTypes.bool.def(false),
     trigger: VueTypes.oneOf(['click', 'hover', 'focus', 'active']).def('click'),
     placement: VueTypes.oneOf(Popper.placements).def('auto'),
     positionFixed: VueTypes.bool.def(false),
@@ -12,21 +24,14 @@ export default {
     removeOnDestroy: VueTypes.bool.def(false),
 
     // modifiers
-    shift: VueTypes.shape({
-      order: VueTypes.number,
-      enabled: VueTypes.bool,
-    }).loose.def({ order: 100, enabled: true }),
+    shift: VueTypes.shape({ enabled: VueTypes.bool }).loose.def({
+      enabled: true,
+    }),
     offset: VueTypes.shape({
-      order: VueTypes.number,
       enabled: VueTypes.bool,
       offset: VueTypes.oneOfType([VueTypes.string, VueTypes.number]),
-    }).loose.def({
-      order: 200,
-      enabled: true,
-      offset: 0,
-    }),
+    }).loose.def({ enabled: true, offset: 0 }),
     preventOverflow: VueTypes.shape({
-      order: VueTypes.number,
       enabled: VueTypes.bool,
       priority: VueTypes.array,
       padding: VueTypes.number,
@@ -35,27 +40,19 @@ export default {
         VueTypes.object,
       ]),
     }).loose.def({
-      order: 300,
       enabled: true,
       priority: ['left', 'right', 'top', 'bottom'],
       padding: 5,
       boundariesElement: 'scrollParent',
     }),
-    keepTogether: VueTypes.shape({
-      order: VueTypes.number,
-      enabled: VueTypes.bool,
-    }).loose.def({ order: 400, enabled: true }),
+    keepTogether: VueTypes.shape({ enabled: VueTypes.bool }).loose.def({
+      enabled: true,
+    }),
     arrow: VueTypes.shape({
-      order: VueTypes.number,
       enabled: VueTypes.bool,
       element: VueTypes.oneOfType([VueTypes.string, VueTypes.object]),
-    }).loose.def({
-      order: 500,
-      enabled: true,
-      element: '[x-arrow]',
-    }),
+    }).loose.def({ enabled: true, element: '[x-arrow]' }),
     flip: VueTypes.shape({
-      order: VueTypes.number,
       enabled: VueTypes.bool,
       behavior: VueTypes.oneOfType([
         VueTypes.oneOf(['flip', 'clockwise', 'counterclockwise']),
@@ -67,22 +64,18 @@ export default {
         VueTypes.object,
       ]),
     }).loose.def({
-      order: 600,
       enabled: true,
       behavior: 'flip',
       padding: 5,
       boundariesElement: 'viewport',
     }),
-    inner: VueTypes.shape({
-      order: VueTypes.number,
-      enabled: VueTypes.bool,
-    }).loose.def({ order: 700, enabled: false }),
-    hide: VueTypes.shape({
-      order: VueTypes.number,
-      enabled: VueTypes.bool,
-    }).loose.def({ order: 800, enabled: true }),
+    inner: VueTypes.shape({ enabled: VueTypes.bool }).loose.def({
+      enabled: false,
+    }),
+    hide: VueTypes.shape({ enabled: VueTypes.bool }).loose.def({
+      enabled: true,
+    }),
     computeStyle: VueTypes.shape({
-      order: VueTypes.number,
       enabled: VueTypes.bool,
       gpuAcceleration: VueTypes.bool,
       // prettier-ignore
@@ -90,7 +83,6 @@ export default {
       // prettier-ignore
       y: VueTypes.oneOf(['\'left\'', '\'right\''])
     }).loose.def({
-      order: 850,
       enabled: true,
       gpuAcceleration: true,
       // prettier-ignore
@@ -98,20 +90,107 @@ export default {
       // prettier-ignore
       y: '\'left\''
     }),
-    applyStyle: VueTypes.shape({
-      order: VueTypes.number,
-      enabled: VueTypes.bool,
-      onLoad: VueTypes.func,
-    }).loose.def({ order: 900, enabled: true }),
+    applyStyle: VueTypes.shape({ enabled: VueTypes.bool }).loose.def({
+      enabled: true,
+    }),
   },
 
   data() {
     return {
-      vVisible: false,
+      vVisible: this.defaultVisible,
     };
   },
 
+  computed: {
+    cVisible() {
+      return (
+        !this.disabled &&
+        ((typeof this.visible === 'undefined' ? this.vVisible : this.visible) ||
+          this.always)
+      );
+    },
+  },
+
+  watch: {
+    cVisible(val) {
+      if (val) this._updatePopper();
+
+      this.$emit(val ? 'show' : 'hide');
+      this.$emit('change', val);
+    },
+  },
+
+  mounted() {
+    if (this.cVisible) this._updatePopper();
+  },
+
   render() {
-    return null;
+    // let children = this.$slots.default && this.$slots.default[0];
+    // let content = this.$slots.content && this.$slots.content[0];
+    //
+    // if (children) children = cloneElement(children, { ref: 'reference' });
+    // if (content)
+    //   content = cloneElement(content, {
+    //     ref: 'popper',
+    //     directives: [{ name: 'show', value: this.cVisible }],
+    //   });
+    // {/*{children}*/}
+    // {/*<transition name="fade">{content}</transition>*/}
+    const wrapperData = {
+      ref: 'popper',
+      directives: [{ name: 'show', value: this.cVisible }],
+    };
+
+    return (
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+        <div ref="reference">{this.$slots.default}</div>
+        <transition name="fade">
+          <div {...wrapperData}>{this.$slots.content}</div>
+        </transition>
+      </div>
+    );
+  },
+
+  methods: {
+    _createPopper() {
+      const reference = this.$refs.reference;
+      const popper = this.$refs.popper;
+
+      if (!reference || !popper) return;
+
+      this.popperJS = new Popper(reference, popper, {
+        placement: this.placement,
+        positionFixed: this.positionFixed,
+        eventsEnabled: this.eventsEnabled,
+        removeOnDestroy: this.removeOnDestroy,
+        onCreate: this._handleCreate,
+        onUpdate: this._handleUpdate,
+        modifiers: {
+          shift: this.shift,
+          offset: this.offset,
+          preventOverflow: this.preventOverflow,
+          keepTogether: this.keepTogether,
+          arrow: this.arrow,
+          flip: this.flip,
+          inner: this.inner,
+          hide: this.hide,
+          applyStyle: this.applyStyle,
+          computeStyle: this.computeStyle,
+        },
+      });
+    },
+
+    _updatePopper() {
+      this.popperJS ? this.popperJS.update() : this._createPopper();
+    },
+
+    _handleCreate() {
+      this.$nextTick(this._updatePopper);
+      this.$emit('create');
+    },
+
+    _handleUpdate() {
+      this.$emit('update');
+    },
   },
 };
